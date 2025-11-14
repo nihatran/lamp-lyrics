db206b4d4354a2635521c422dbb545305bf19c70
nihatran <89045243+nihatran@users.noreply.github.com>
2025-11-14 18:31:36 -0500

    Improve responsiveness of hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col xl:h-svh w-full justify-center items-center px-10 py-5 xl:p-0">
-      <div className="xl:h-svh xl:w-[70vw] w-full flex flex-col items-center">
+    <div className="flex flex-col w-full justify-center items-center px-10">
+      <div className="xl:w-[70vw] w-full flex flex-col items-center">
         <h1 className="text-[clamp(2.4rem,9vw,14rem)] font-medium xl:font-normal">
           Lyrics From Lamp
         </h1>
-        <div className="flex flex-col xl:flex-row xl:gap-30 gap-4 mt-3 xl:mt-0">
+        <div className="flex flex-col xl:flex-row gap-10 mt-3 xl:mt-0 items-center">
           <div className="flex flex-col gap-4">
             <Image
               src="/images/lamp1.jpg"
@@ -43,8 +43,8 @@ export default function Hero() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col-reverse xl:flex-col xl:w-[18vw] text-sm lg:text-lg md:text-base xl:text-xl xl:justify-between items-center xl:items-start">
-            <div className="flex flex-col xl:gap-6 gap-4">
+          <div className="flex xl:flex-col flex-col-reverse items-center xl:gap-25 gap-10 xl:w-[20vw] md:w-[60vw] w-full">
+            <div className="flex flex-col xl:gap-6 gap-4 xl:w-[90%]">
               <p>
                 Nostalgia and longing are interwoven in the dreamy songs of
                 Japanese indie pop band Lamp.
@@ -58,14 +58,13 @@ export default function Hero() {
                 listening to on a quiet Sunday morning.
               </p>
             </div>
-            <div className="relative w-[250px] aspect-square sm:w-[445px] mb-4 xl:mb-0">
-              <Image
-                src="/images/lamp2.jpg"
-                fill
-                alt="lamp band group photo"
-                sizes="(max-width:440px) 250px, 445px"
-              ></Image>
-            </div>
+            <Image
+              src="/images/lamp2.jpg"
+              height={440}
+              width={440}
+              alt="lamp band group photo"
+              sizes="(max-width:440px) 250px, 445px"
+            ></Image>
           </div>
         </div>
       </div>
